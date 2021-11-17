357768c37a064e26fe93c780854c759f41dbc400
Juan Jose <josejuan250203@gmail.com>
2021-11-16 23:01:07 -0500

    Modulo de facvturas

--- static/js/facture/index.js
+++ static/js/facture/index.js
@@ -4,6 +4,9 @@ $(document).ready(function (){
     });
     
     viewButtons(false)
+
+    listTableProducts();
+    calculateTotal();
 });
 
 
@@ -118,13 +121,18 @@ function getStorage(){
 
 function addProduct(){
     //datos
-    let product = $('#product').val()
+    let nameProduct = $('#product option:selected').attr("nameP");
+    let valueProduct = $('#product option:selected').attr("valueP");
     let amount = $('#amount').val()
     
     // Json
+    let total = parseInt(valueProduct) * parseInt(amount);
     const productObj = {
-        product, // product: product
-        amount
+        id: uuid.v4(), 
+        nameProduct,
+        valueProduct,
+        amount,
+        total
     }
 
     // Actualizar Storage
@@ -132,15 +140,132 @@ function addProduct(){
 
     let productsArray = [...productsStorage, productObj];
     
-    // Conver
-    const jsonString = JSON.stringify(productsArray);
-    
-    //Guardar localstorage
-    localStorage.setItem("products", jsonString)
+    // Actualizar Storage
+    updateStorage(productsArray)
 
     // listar tabla
+    listTableProducts()
 
     // Calcular valores
+    calculateTotal()
     
     // Limpiar inputs
 }
+
+function updateStorage(data){
+    // Conversion de array a string
+    const jsonString = JSON.stringify(data);
+        
+    //Guardar localstorage
+    localStorage.setItem("products", jsonString)
+}
+
+function listTableProducts(){
+    $('#listProducts tbody').empty();
+
+    const products = getStorage();
+    products.forEach(product => {
+        let row = '<tr>';
+        row += '<td class="text-center">' + product.amount +'</td>'
+        row += '<td class="text-center">' + product.nameProduct +'</td>'
+        row += '<td class="text-center"> $' + numeral(product.valueProduct).format() +'</td>'
+        row += '<td class="text-center"> $' + numeral(product.amount * product.valueProduct).format() +'</td>'
+        row += 
+            `<td class="text-center">
+                <button class="btn" type="button"
+                    onclick="deleteProduct('${product.id}')"
+                >
+                    <i
+                        title="Eliminar Producto"
+                        class="fas fa-times text-danger">
+                    </i>
+                </button>
+            </td>`
+        row += '</tr>'
+        $('#listProducts tbody').append(row);
+    });
+}
+
+function calculateTotal(){
+    // Extraer productos del localstorage
+    const products = getStorage();
+
+    // Calculo total
+    let total = 0;
+    products.forEach(product => {
+        total += product.total;
+    });
+    
+    // Enviar al localStorage
+    let stringTotal = total.toString();
+    
+    localStorage.setItem('totalProducts', stringTotal)
+    
+    // Convertir a miles
+    let totalString = numeral(total).format();    
+    
+    // Mostrar valor en el DOM
+    $('#totalPayment').empty();
+
+    const spanText = $(`
+        <span class="text-dark">
+            <b>Total: </b>
+        </span>
+    `);
+
+    const spanTotal = $(`
+        <span class="text-secondary">
+            <b>$${totalString}</b>
+        </span>
+    `);
+    
+    $('#totalPayment').append(spanText);
+    $('#totalPayment').append(spanTotal);
+}
+
+function deleteProduct(id){
+    console.log("Entra")
+    console.log(id)
+    // Traer datos del Storage
+    const products = getStorage();
+
+    // Eliminar
+    const result = products.filter( product => product.id != id );
+    
+    // Actualizar Storage
+    updateStorage(result);
+
+    // Listar productos
+    listTableProducts();
+
+    // Actualizar calculo
+    calculateTotal();
+}
+
+$( "#payment" ).keyup(function() {
+    let payment = parseInt($( "#payment" ).val());
+    let totalProducts = parseInt(localStorage.getItem('totalProducts')); 
+    let balance = totalProducts - payment
+    let balanceString = balance.toString()
+    let balanceStrFormat = numeral(balance).format()
+    localStorage.setItem('totalBalance', balanceString)
+    
+    // Mostrar valor en el DOM
+    $('#balance').empty();
+    
+
+    const spanText = $(`
+        <span class="text-dark">
+            <b>Total: </b>
+        </span>
+    `);
+
+    const spanTotal = $(`
+        <span class="text-secondary">
+            <b>$${balanceStrFormat}</b>
+        </span>
+    `);
+
+    $('#balance').append(spanText);
+    $('#balance').append(spanTotal);
+  });
